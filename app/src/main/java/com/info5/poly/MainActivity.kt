908663e0eecaf3232c5180d74acf84cf105500c9
Julian Royet <julianroyet@free.fr>
2023-02-01 20:35:39 +0100

    execution python

--- app/src/main/java/com/info5/poly/MainActivity.kt
+++ app/src/main/java/com/info5/poly/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.info5.poly
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import com.info5.poly.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val JS_OBJ_NAME = "AndroidAPI"
+    private lateinit var python: PythonExecutor
 
     private inner class WebAndroidAPI {
-        private var isRecording: Boolean = false;
+        private var isRecording: Boolean = false
 
         @JavascriptInterface
         fun setRecording(recording: Boolean){
-            this.isRecording = recording;
-            Log.d("Poly Debug", "recording: " + this.isRecording);
+            this.isRecording = recording
+            Log.d("Poly Debug", "recording: " + this.isRecording)
+
+
+            // TEST =========
+            var code = "def fibonacci_of(n):\n"
+            code += "   if n in {0, 1}:  # Base case\n"
+            code += "       return n\n"
+            code += "   return n\n"
+            code += "\n"
+            code += "print([fibonacci_of(n) for n in range(10)])\n"
+
+            python.fromString(code){
+                Log.d("PythonOutput", it)
+            }
+            //================
         }
     }
 
@@ -33,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         private fun escapeJS(str: String): String{
             return str
                 .replace("'", "\'")
-                .replace("\n", "\\n");
+                .replace("\n", "\\n")
         }
         fun addMessage(user: Boolean){
             webView.evaluateJavascript("addMessage(${user};") { messageID ->
@@ -49,16 +59,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        python = PythonExecutor(applicationContext)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val webView: WebView = findViewById(R.id.webview)
         webView.settings.javaScriptEnabled = true
         webView.addJavascriptInterface(WebAndroidAPI(), JS_OBJ_NAME)
-        webView.loadUrl("file:///android_asset/web/poly.html");
+        webView.loadUrl("file:///android_asset/web/poly.html")
 
         webView.webChromeClient = object: WebChromeClient() {
             override fun onConsoleMessage(message: String?, lineNumber: Int, sourceID: String?) {
