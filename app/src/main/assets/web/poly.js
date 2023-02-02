6551b75a17a2d6503df2d73b14a66ae0a437775f
zhangkem <Keming.zhang@etu.univ-grenoble-alpes.fr>
2023-02-02 15:37:30 +0100

    chat bubble

--- app/src/main/assets/web/poly.js
+++ app/src/main/assets/web/poly.js
@@ -1,5 +1,25 @@
 var recording = false;
 document.getElementById("speak-bubble").addEventListener("click", () => {
     recording = !recording;
-    AndroidAPI.setRecording(recording);
+    //AndroidAPI.setRecording(recording);
+    addQuestion("Hello???");
+    addResponse("Hello!!!");
 })
+
+function addQuestion(question){
+    const template = document.querySelector("template");
+    const clone = template.content.firstElementChild.cloneNode(true);
+    const p =  clone.querySelectorAll("p");
+    p[0].innerHTML = question;
+    const main = document.querySelector("main");
+    main.appendChild(clone);
+}
+
+function addResponse(response){
+    const template = document.querySelector("template");
+    const clone = template.content.lastElementChild.cloneNode(true);
+    const p =  clone.querySelectorAll("p");
+    p[0].innerHTML = response;
+    const main = document.querySelector("main");
+    main.appendChild(clone);
+}
