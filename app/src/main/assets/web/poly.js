555dd9febcee9d02ba91072b3769896d5626c19c
Mirette Guirguis <mirette.guirguis@etu.univ-grenoble-alpes.fr>
2023-02-03 15:36:28 +0100

    ajout du boutton de stop recording+la bar de parole+ajustement du scroll down

--- app/src/main/assets/web/poly.js
+++ app/src/main/assets/web/poly.js
@@ -1,20 +1,69 @@
 var recording = false;
+ 
 document.getElementById("speak-bubble").addEventListener("click", () => {
-    recording = !recording;
+
     //AndroidAPI.setRecording(recording);
+    if (recording) {
+        stopRecording();
+    }
+    else {
+        startRecording();
+    }
     addMessage(true, "Hello???");
     addMessage(false, "Hello!!!");
 })
 
-function addMessage(isUser, msg){
+function addMessage(isUser, msg) {
     var template;
-    if(isUser)
+    if (isUser)
         template = document.querySelector("template").content.querySelector(".user-message");
-    else 
+    else
         template = document.querySelector("template").content.querySelector(".assistant-message");
     const clone = template.cloneNode(true);
-    const p =  clone.querySelectorAll("p");
+    const p = clone.querySelectorAll("p");
     p[0].innerHTML = msg;
     const main = document.querySelector("main");
     main.appendChild(clone);
+    document.querySelector("main").scrollTo(0, document.querySelector("main").scrollHeight);
+
 }
+function startRecording() {
+    recording = true;
+    const speakingBar = document.getElementById("speaking-bar");
+    speakingBar.style.width = "100%";
+    document.getElementsByTagName("footer")[0].style.height = "30vh";
+    document.getElementById("speak-icon").innerHTML = "send";
+
+    AdujstSpeakingBar();
+
+
+}
+
+function stopRecording() {
+    recording = false;
+    const speakingBar = document.getElementById("speaking-bar");
+    speakingBar.style.width = "0%";
+    document.getElementsByTagName("footer")[0].style.height = "8vh";
+    document.getElementById("speak-icon").innerHTML = "mic";
+
+}
+
+function AdujstSpeakingBar() {
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.start();
+
+    // Set up the event listener to update the width of the bar based on the audio levels
+    recognition.addEventListener("result", (event) => {
+        const result = event.results[event.resultIndex];
+        const transcript = result[0].transcript;
+        console.log(transcript);
+        const confidence = result[0].confidence;
+        const speakingBar = document.getElementById("speaking-bar");
+        speakingBar.style.width = `${confidence * 100}%`;
+    })
+}
+
+
+
+
