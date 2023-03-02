1741895a40c4846381360125b6ab864e7e84cc62
Julian Royet <julianroyet@free.fr>
2023-03-02 23:36:09 +0100

    reconnaissance de voix ok

--- app/src/main/assets/web/poly.js
+++ app/src/main/assets/web/poly.js
@@ -1,111 +1,63 @@
 
-var recording = false;
-const feelings = {
-    satisfied: "sentiment_satisfied",
-    dissatisfied: "sentiment_dissatisfied",
-    neutral: "sentiment_neutral"
-}
-var user_feeling = feelings.neutral;
+var listening = false;
 
-//variable pour tester l'avatar
-var feeling_count = 1;
 var user_theme;
 
+var lastMessage = null;
 
 document.getElementById("speak-bubble").addEventListener("click", () => {
+    AndroidAPI.setListening(!listening);
 
-    //AndroidAPI.setRecording(recording);
-
-   if (recording) {
-        stopRecording();
-    }
-    else {
-        startRecording();
-    }
-
-    feeling_count = feeling_count % 3;
-    switch (feeling_count) {
-        case 1: changeUserFeeling("happy"); break;
-        case 2: changeUserFeeling("sad"); break;
-        case 0: changeUserFeeling("neutral"); break;
-    }
-    feeling_count++;
-
-    //AndroidAPI.phoneCall("tel:0767642068");
-    //AndroidAPI.handlePermissions();
+    setListening(!listening);
 })
 
-function addMessage(isUser, msg) {
+function addMessage(isUser) {
     var template;
     if (isUser)
         template = document.querySelector("template").content.querySelector(".user-message");
     else
         template = document.querySelector("template").content.querySelector(".assistant-message");
-    const clone = template.cloneNode(true);
-    const p = clone.querySelectorAll("p");
-    p[0].innerHTML = msg;
+    lastMessage = template.cloneNode(true);
+    lastMessage.querySelector("p").innerHTML = "🎙️";
     const main = document.querySelector("main");
-    main.appendChild(clone);
+    main.appendChild(lastMessage);
     document.querySelector("main").scrollTo(0, document.querySelector("main").scrollHeight);
-
 }
-function startRecording() {
-    recording = true;
+
+function editMessage(msg){
+    lastMessage.querySelector("p").innerHTML = msg;
+}
+
+function deleteMessage(msg){
+    lastMessage.remove();
+}
+
+
+function setListening(l){
+    listening = l;
+
+    if (l) {
+        startListening();
+    }
+    else {
+        stopListening();
+    }
+}
+function startListening() {
     const speakingBar = document.getElementById("speaking-bar");
     speakingBar.style.width = "100%";
     document.getElementsByTagName("footer")[0].style.height = "30vh";
-    document.getElementById("speak-icon").innerHTML = "send";
-
-    //Audio recording
-    AndroidAPI.onRecognizeSpeech()
-    recognizedText = AndroidAPI.getMessage();
-    addMessage(true, recognizedText);
-
-    //AdujstSpeakingBar();
+    document.getElementById("speak-icon").innerHTML = "close";
 }
 
-function stopRecording() {
-    recording = false;
+function stopListening() {
     const speakingBar = document.getElementById("speaking-bar");
     speakingBar.style.width = "0%";
     document.getElementsByTagName("footer")[0].style.height = "8vh";
     document.getElementById("speak-icon").innerHTML = "mic";
 
-    addMessage(false, "Réponse de Poly");
-
 }
 
-function AdujstSpeakingBar() {
-    const recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.start();
-
-    // Set up the event listener to update the width of the bar based on the audio levels
-    recognition.addEventListener("result", (event) => {
-        const result = event.results[event.resultIndex];
-        const transcript = result[0].transcript;
-        console.log(transcript);
-        const confidence = result[0].confidence;
-        const speakingBar = document.getElementById("speaking-bar");
-        speakingBar.style.width = `${confidence * 100}%`;
-    })
-}
-
-
-//feeling : l'entree que chatGPT va nous donner 
-function changeUserFeeling(feeling) {
-    //adapter le switch selon le type de l'entree que chatGPT va nous rendre
-
-    switch (feeling) {
-        case "happy": user_feeling = feelings.satisfied; break;
-        case "sad": user_feeling = feelings.dissatisfied; break;
-        case "neutral": user_feeling = feelings.neutral; break;
-    }
-
-    document.getElementById("feeling_icon").innerHTML = user_feeling;
-
-
-}
 $(".dropdown-menu li a").click(function () {
     var selText = $(this).text();
     $(this).parents('.dropdown').find('.dropdown-toggle').html(selText + ' <span class="theme"></span>');
@@ -148,13 +100,6 @@ document.getElementById("save-button").addEventListener("click", () => {
             }
             break;
     }
-
-
-
-
-
-
-
 
 })
 
--- app/src/main/java/com/info5/poly/MainActivity.kt
+++ app/src/main/java/com/info5/poly/MainActivity.kt
@@ -11,157 +11,79 @@ import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.text.Editable
 import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.material.internal.ContextUtils.getActivity
 import com.info5.poly.databinding.ActivityMainBinding
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_SPEECH_TO_TEXT = 1
-    private var speechMessage : String = "Message par défaut"
     private lateinit var binding: ActivityMainBinding
     val JS_OBJ_NAME = "AndroidAPI"
-    private lateinit var python: PythonExecutor
+    private var speechRecognizer: SpeechRecognizer? = null
+    private lateinit var speechRecognizerIntent: Intent
+    private lateinit var api: WebAPI;
+    private val permissionsToAcquire: MutableList<String> = mutableListOf(
+        Manifest.permission.RECEIVE_SMS,
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
 
-    private inner class WebAndroidAPI {
-        private var isRecording: Boolean = false
-        private var speechRecognizer : SpeechRecognizer? = null //Speech recognizer
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        if(it)
+            permissionsToAcquire.removeAt(0)
 
-        @JavascriptInterface
-        fun setRecording(recording: Boolean){
-            this.isRecording = recording
-            Log.d("Poly Debug", "recording: " + this.isRecording)
-
-
-            // TEST =========
-            var code = "def fibonacci_of(n):\n"
-            code += "   if n in {0, 1}:  # Base case\n"
-            code += "       return n\n"
-            code += "   return n\n"
-            code += "\n"
-            code += "print([fibonacci_of(n) for n in range(10)])\n"
-
-            python.fromString(code){
-                Log.d("PythonOutput", it)
-            }
-            //================
-        }
-
-        /*@JavascriptInterface
-        fun handlePermissions () {
-            // Phone call
-            if (ContextCompat.checkSelfPermission(applicationContext,android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                val requestCode = 1
-                ActivityCompat.requestPermissions(
-                    this@MainActivity, arrayOf(android.Manifest.permission.CALL_PHONE),
-                    requestCode
-                )
-            }
-
-            // Audio recording
-            if (ContextCompat.checkSelfPermission
-                    (applicationContext, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
-            {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    val recordAudioRequestCode = 1
-                    ActivityCompat.requestPermissions(
-                        this@MainActivity, arrayOf(Manifest.permission.RECORD_AUDIO),
-                        recordAudioRequestCode
-                    )
-                }
-            }
-        }*/
-
-        @JavascriptInterface
-        fun phoneCall(phoneNumber: String) {
-            val callIntent: Intent = Uri.parse(phoneNumber).let { number ->
-                Intent(Intent.ACTION_CALL, number)
-            }
-            if (ContextCompat.checkSelfPermission(applicationContext,android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                val requestCode = 1
-                ActivityCompat.requestPermissions(
-                    this@MainActivity, arrayOf(android.Manifest.permission.CALL_PHONE),
-                    requestCode
-                )
-            } else {
-                try {
-                    startActivity(callIntent)
-                } catch (e: ActivityNotFoundException) {
-                    // Define what your app should do if no activity can handle the intent.
-                }
-            }
-        }
-
-        // Enable speech recognition activity
-        @JavascriptInterface
-        fun onRecognizeSpeech() {
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say Something...")
-
-            if (intent.resolveActivity(packageManager) != null) {
-                try {
-                    startActivityForResult(intent, REQUEST_SPEECH_TO_TEXT)
-                }
-                catch (exp : ActivityNotFoundException) {
-                    Toast.makeText(applicationContext, "Device not supported", Toast.LENGTH_SHORT).show()
-                }
-            }
-            else {
-                Toast.makeText(applicationContext,
-                    "There is no app to handle the voice recognition",
-                    Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Getting a message from the user's speech
-        @JavascriptInterface
-        fun getMessage () : String {
-            return speechMessage
-        }
+        requestPermissions()
     }
 
+    private inner class WebAndroidAPI {
+        private var isListening: Boolean = false
 
-    // Handle the result of activities
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_SPEECH_TO_TEXT && resultCode == RESULT_OK) {
-            //speechMessage = data!!.data
-            val response = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            speechMessage = response!![0]
+        @JavascriptInterface
+        fun setListening(listening: Boolean){
+            runOnUiThread {
+                if(this.isListening && !listening){
+                    stop_listening()
+                }
+                else if(!this.isListening && listening){
+                    listen_voice()
+                }
+
+                this.isListening = listening
+            }
         }
     }
 
     private inner class WebAPI(private val webView: WebView) {
         private fun escapeJS(str: String): String{
             return str
-                .replace("'", "\'")
+                .replace("'", "\\'")
                 .replace("\n", "\\n")
         }
+
         fun addMessage(user: Boolean){
-            webView.evaluateJavascript("addMessage(${user};") { messageID ->
-
-            }
-        }
-        fun editMessage(user: Boolean, id: Int, msg: String){
-            webView.evaluateJavascript("editMessage(${user}, ${id}, '${escapeJS(msg)}';") {}
+            webView.evaluateJavascript("addMessage(${user});") {}
         }
 
-        fun setLoading(loading: Boolean){
-            webView.evaluateJavascript("setLoading(${loading};") {}
+        fun editMessage(msg: String){
+            webView.evaluateJavascript("editMessage('${escapeJS(msg)}');") {}
+        }
+
+        fun deleteMessage(){
+            webView.evaluateJavascript("deleteMessage();") {}
+        }
+
+        fun setListening(listening: Boolean){
+            webView.evaluateJavascript("setListening(${listening});") {}
         }
     }
 
@@ -169,7 +91,6 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        python = PythonExecutor(applicationContext)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -196,21 +117,114 @@ class MainActivity : AppCompatActivity() {
                 Log.d("WebViewConsole", message!!)
             }
         }
+        initSpeechRecognition()
+        api = WebAPI(webView)
+    }
+
+    fun initSpeechRecognition(){
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak something")
+
+        speechRecognizer!!.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle) {
+                // Called when the recognizer is ready for the user to start speaking
+            }
+
+            override fun onBeginningOfSpeech() {
+                // Called when the user starts speaking
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+                // Called when the volume of the user's speech changes
+            }
+
+            override fun onBufferReceived(buffer: ByteArray) {
+                // Called when the recognizer has received audio input
+            }
+
+            override fun onEndOfSpeech() {
+                api.setListening(false)
+            }
+
+            override fun onError(error: Int) {
+                // Called when an error occurs
+            }
+
+            override fun onResults(results: Bundle) {
+                // Called when the recognizer has successfully recognized the user's speech
+                val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                if (matches != null && matches.size > 0) {
+                    val spokenText = matches[0]
+                    // Do something with the recognized text
+
+                    api.editMessage(spokenText)
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle) {
+                val matches = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                if (matches != null && matches.size > 0) {
+                    val spokenText = matches[0]
+                    api.editMessage(spokenText)
+                }
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle) {
+                // Called when a recognition event occurs
+            }
+        })
+    }
+
+    fun phoneCall(phoneNumber: String) {
+        val callIntent: Intent = Uri.parse(phoneNumber).let { number ->
+            Intent(Intent.ACTION_CALL, number)
+        }
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            val requestCode = 1
+            ActivityCompat.requestPermissions(
+                this@MainActivity, arrayOf(Manifest.permission.CALL_PHONE),
+                requestCode
+            )
+        } else {
+            try {
+                startActivity(callIntent)
+            } catch (e: ActivityNotFoundException) {
+                // Define what your app should do if no activity can handle the intent.
+            }
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun listen_voice() {
+        api.addMessage(true)
+
+        speechRecognizer?.startListening(speechRecognizerIntent)
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        val recordAudioRequestCode = 1
-        if (requestCode == recordAudioRequestCode
-            && grantResults.isNotEmpty())
-        {
-            Toast.makeText(this, "Permission Granted...", Toast.LENGTH_SHORT).show()
+    fun stop_listening() {
+        api.deleteMessage()
+        speechRecognizer?.cancel()
+
+    }
+
+    private fun requestPermissions(){
+        if(permissionsToAcquire.isEmpty())
+            return
+        var permission = permissionsToAcquire[0]
+
+        while(ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED){
+            permissionsToAcquire.removeAt(0)
+            if(permissionsToAcquire.isEmpty())
+                return
+            permission = permissionsToAcquire[0]
         }
+        permissionLauncher.launch(permission)
+
     }
 
 
